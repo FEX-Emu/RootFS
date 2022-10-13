53a705aa72c4ed0997a0f568a64624bbf1490571
Ryan Houdek <Sonicadvance1@gmail.com>
2022-10-12 20:47:38 -0700

    update mesa build

--- build_install_mesa.sh
+++ build_install_mesa.sh
@@ -34,6 +34,9 @@ apt-get build-dep -y mesa
 # Move to /root/
 cd /root
 
+# Clone meson
+git clone --depth=1 --branch 0.63 https://github.com/mesonbuild/meson.git
+
 # Build and install DRM
 git clone --depth=1 --branch libdrm-2.4.110 https://gitlab.freedesktop.org/mesa/drm.git
 cd drm
@@ -42,7 +45,7 @@ mkdir Build
 mkdir Build_x86
 
 cd Build
-meson -Dprefix=/usr  -Dlibdir=/usr/lib/x86_64-linux-gnu \
+/root/meson/meson.py -Dprefix=/usr  -Dlibdir=/usr/lib/x86_64-linux-gnu \
 	-Dbuildtype=release \
 	-Db_ndebug=true \
 	-Dvc4=true -Dtegra=true -Dfreedreno=true -Dexynos=true -Detnaviv=true \
@@ -56,7 +59,7 @@ ninja install
 cd ../
 cd Build_x86
 
-meson -Dprefix=/usr -Dlibdir=/usr/lib/i386-linux-gnu \
+/root/meson/meson.py -Dprefix=/usr -Dlibdir=/usr/lib/i386-linux-gnu \
 	-Dbuildtype=release \
 	-Db_ndebug=true \
 	-Dvc4=true -Dtegra=true -Dfreedreno=true -Dexynos=true -Detnaviv=true \
@@ -72,17 +75,19 @@ ninja install
 cd /root
 
 # Build and install mesa
-git clone --depth=1 --branch mesa-22.2.0-fex https://gitlab.freedesktop.org/sonicadvance1/mesa.git
+git clone https://gitlab.freedesktop.org/mesa/mesa.git
 
 cd mesa
+git checkout f8f4648cac19b5d3e67596f1b8c155c61f4f1c32
 mkdir Build
 mkdir Build_x86
 
-export GALLIUM_DRIVERS="r300,r600,radeonsi,nouveau,virgl,svga,swrast,iris,kmsro,v3d,vc4,freedreno,etnaviv,tegra,lima,panfrost,zink,asahi"
-export VULKAN_DRIVERS="amd,intel,freedreno,swrast,broadcom,panfrost"
+export GALLIUM_DRIVERS="r300,r600,radeonsi,nouveau,virgl,svga,swrast,iris,kmsro,v3d,vc4,freedreno,etnaviv,tegra,lima,panfrost,zink,asahi,d3d12"
+export VULKAN_DRIVERS="amd,intel,freedreno,swrast,broadcom,panfrost,virtio-experimental"
 
+# llvmspirvlib-dev is llvm-13 while llvm-14 is used in Ubuntu 22.04. Two version need to match. Can't use rusticl because of this.
 cd Build
-meson -Dprefix=/usr  -Dlibdir=/usr/lib/x86_64-linux-gnu \
+/root/meson/meson.py -Dprefix=/usr  -Dlibdir=/usr/lib/x86_64-linux-gnu \
 	-Dbuildtype=release \
 	-Db_ndebug=true \
 	-Dgallium-drivers=$GALLIUM_DRIVERS \
@@ -99,7 +104,8 @@ ninja install
 cd ../
 cd Build_x86
 
-meson -Dprefix=/usr -Dlibdir=/usr/lib/i386-linux-gnu \
+# llvmspirvlib-dev:i386 doesn't exist so rusticl can't be used on 32-bit
+/root/meson/meson.py -Dprefix=/usr -Dlibdir=/usr/lib/i386-linux-gnu \
 	-Dbuildtype=release \
 	-Db_ndebug=true \
 	-Dgallium-drivers=$GALLIUM_DRIVERS \
