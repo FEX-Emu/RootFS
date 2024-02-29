1f2c04843154a981d4585a956e837dec38433abd
Ryan Houdek <Sonicadvance1@gmail.com>
2024-02-29 09:00:08 -0800

    [Ubuntu 23.10] Update mesa

--- Scripts/Ubuntu_23_10/build_install_mesa.sh
+++ Scripts/Ubuntu_23_10/build_install_mesa.sh
@@ -7,7 +7,9 @@ apt-get update
 cd /root
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y git ninja-build clang gcc-i686-linux-gnu g++-i686-linux-gnu \
-  llvm-dev libvulkan-dev libpciaccess-dev libglvnd-dev cargo libclang-dev
+  llvm-dev libvulkan-dev libpciaccess-dev libglvnd-dev cargo libclang-dev \
+  spirv-tools \
+  cargo
 
 apt-get install -y libvulkan-dev:i386 libelf-dev:i386 libwayland-dev:i386 libwayland-egl-backend-dev:i386 \
   libpciaccess-dev:i386 \
@@ -33,17 +35,16 @@ apt-get install -y libvulkan-dev:i386 libelf-dev:i386 libwayland-dev:i386 libway
 apt-get build-dep -y mesa
 
 # We are using clang/llvm 16, remove spirvlib15
-apt-get remove -y libllvmspirvlib-15-dev libllvmspirvlib-15-dev:i386
 apt-get install -y libllvmspirvlib-16-dev libllvmspirvlib-16-dev:i386
 
 # Move to /root/
 cd /root
 
 # Clone meson
-git clone --depth=1 --branch 1.2.0 https://github.com/mesonbuild/meson.git
+git clone --depth=1 --branch 1.3.1 https://github.com/mesonbuild/meson.git
 
 # Build and install DRM
-git clone --depth=1 --branch libdrm-2.4.110 https://gitlab.freedesktop.org/mesa/drm.git
+git clone --depth=1 --branch libdrm-2.4.119 https://gitlab.freedesktop.org/mesa/drm.git
 cd drm
 
 mkdir Build
@@ -53,7 +54,7 @@ cd Build
 /root/meson/meson.py -Dprefix=/usr  -Dlibdir=/usr/lib/x86_64-linux-gnu \
   -Dbuildtype=release \
   -Db_ndebug=true \
-  -Dvc4=true -Dtegra=true -Dfreedreno=true -Dexynos=true -Detnaviv=true \
+  -Dvc4=enabled -Dtegra=enabled -Dfreedreno=enabled -Dexynos=enabled -Detnaviv=enabled \
   -Dc_args="-mfpmath=sse -msse -msse2 -mstackrealign" \
   -Dcpp_args="-mfpmath=sse -msse -msse2 -mstackrealign" \
   ..
@@ -67,7 +68,7 @@ cd Build_x86
 /root/meson/meson.py -Dprefix=/usr -Dlibdir=/usr/lib/i386-linux-gnu \
   -Dbuildtype=release \
   -Db_ndebug=true \
-  -Dvc4=true -Dtegra=true -Dfreedreno=true -Dexynos=true -Detnaviv=true \
+  -Dvc4=enabled -Dtegra=enabled -Dfreedreno=enabled -Dexynos=enabled -Detnaviv=enabled \
   -Dc_args="-mfpmath=sse -msse -msse2 -mstackrealign" \
   -Dcpp_args="-mfpmath=sse -msse -msse2 -mstackrealign" \
   --cross-file /root/cross_x86 \
@@ -80,7 +81,7 @@ ninja install
 cd /root
 
 # Build and install mesa
-git clone --depth=1 --branch mesa-23.3.0 https://gitlab.freedesktop.org/mesa/mesa.git
+git clone --depth=1 --branch mesa-24.0.2 https://gitlab.freedesktop.org/mesa/mesa.git
 cd mesa
 mkdir Build
 mkdir Build_x86
@@ -101,6 +102,7 @@ cd Build
   -Dvulkan-drivers=$VULKAN_DRIVERS \
   -Dplatforms=x11,wayland \
   -Dglvnd=true \
+  -Dopencl-external-clang-headers=disabled \
   -Dc_args="-mfpmath=sse -msse -msse2 -mstackrealign" \
   -Dcpp_args="-mfpmath=sse -msse -msse2 -mstackrealign" \
   ..
@@ -111,7 +113,11 @@ ninja install
 cd ../
 cd Build_x86
 
+apt-get install -y spirv-tools:i386 glslang-tools:i386
+
 # No rusticl for 32-bit
+# No asahi for 32-bit since asahi_clc can't cross-compile
+export GALLIUM_DRIVERS="r300,r600,radeonsi,nouveau,virgl,svga,swrast,iris,kmsro,v3d,vc4,freedreno,etnaviv,tegra,lima,panfrost,zink,d3d12"
 /root/meson/meson.py -Dprefix=/usr -Dlibdir=/usr/lib/i386-linux-gnu \
   -Dbuildtype=release \
   -Db_ndebug=true \
@@ -119,6 +125,7 @@ cd Build_x86
   -Dvulkan-drivers=$VULKAN_DRIVERS \
   -Dplatforms=x11,wayland \
   -Dglvnd=true \
+  -Dopencl-external-clang-headers=disabled \
   -Dc_args="-mfpmath=sse -msse -msse2 -mstackrealign" \
   -Dcpp_args="-mfpmath=sse -msse -msse2 -mstackrealign" \
   --cross-file /root/cross_x86 \
@@ -131,3 +138,5 @@ cd /
 
 cargo uninstall bindgen-cli
 apt-get remove -y cargo
+apt-get remove -y spirv-tools:i386 glslang-tools:i386
+apt-get install -y spirv-tools glslang-tools
