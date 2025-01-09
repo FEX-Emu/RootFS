71ae0dd280f188700691c1231c4edeac6ea77c70
Ryan Houdek <Sonicadvance1@gmail.com>
2025-01-08 18:05:55 -0800

    Update Fedora 40

--- Scripts/Fedora/build_install_mesa.sh
+++ Scripts/Fedora/build_install_mesa.sh
@@ -136,7 +136,7 @@ ninja install
 cd /root
 
 # Build and install mesa
-git clone --depth=1 --branch mesa-24.2.0 https://gitlab.freedesktop.org/mesa/mesa.git
+git clone --depth=1 --branch mesa-24.3.3 https://gitlab.freedesktop.org/mesa/mesa.git
 cd mesa
 mkdir Build
 mkdir Build_x86
