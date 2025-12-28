59c6518ea5a706bb647ac09931cd6f076416b69e
Ryan Houdek <Sonicadvance1@gmail.com>
2025-12-27 18:12:27 -0800

    Update Ubuntu 24.04

--- Scripts/Ubuntu_24_04/build_install_mesa.sh
+++ Scripts/Ubuntu_24_04/build_install_mesa.sh
@@ -16,6 +16,14 @@ add-apt-repository -y ppa:kisak/kisak-mesa
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y mesa-va-drivers mesa-vdpau-drivers mesa-vulkan-drivers libglx-mesa0 libgles2-mesa libgl1-mesa-glx libgl1-mesa-dri libegl1-mesa \
+apt-get install -y libgl1 libgl1:i386 mesa-va-drivers mesa-vdpau-drivers mesa-vulkan-drivers libglx-mesa0 libgl1-mesa-dri \
     libegl-mesa0
+
 apt-get upgrade -y
+
+apt-mark hold \
+  libegl-mesa0 libegl-mesa0:i386 libgbm1 libgbm1:i386 libgl1-mesa-dri \
+  libgl1-mesa-dri:i386 libglx-mesa0 libglx-mesa0:i386 libllvm20 libllvm20:i386 \
+  mesa-libgallium mesa-libgallium:i386 mesa-va-drivers mesa-va-drivers:i386 \
+  mesa-vdpau-drivers mesa-vdpau-drivers:i386 mesa-vulkan-drivers \
+  mesa-vulkan-drivers:i386
