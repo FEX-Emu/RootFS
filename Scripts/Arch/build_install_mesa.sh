a2ac76cea2522d5ae382acc9e08ffc54fd1c7b0a
Ryan Houdek <Sonicadvance1@gmail.com>
2026-01-08 16:07:54 -0800

    Arch: Update mesa to 25.3.3

--- Scripts/Arch/build_install_mesa.sh
+++ Scripts/Arch/build_install_mesa.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /root
-pacman --noconfirm -S \
+/root/mega_install_packages.sh \
   git \
   ninja \
   clang \
@@ -21,6 +21,8 @@ pacman --noconfirm -S \
   lib32-spirv-llvm-translator \
   lib32-clang \
   pkgconf \
+  python-setuptools \
+  python-mako \
   python-pycparser
 
 # Install rustup
@@ -31,7 +33,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 cd /root
 
 # Clone meson
-git clone --depth=1 --branch 1.5.1 https://github.com/mesonbuild/meson.git
+git clone --depth=1 --branch 1.10.0 https://github.com/mesonbuild/meson.git
 
 # Build and install DRM
 git clone --depth=1 --branch libdrm-2.4.122 https://gitlab.freedesktop.org/mesa/drm.git
@@ -59,8 +61,6 @@ cd Build_x86
   -Dbuildtype=release \
   -Db_ndebug=true \
   -Dvc4=enabled -Dtegra=enabled -Dfreedreno=enabled -Dexynos=enabled -Detnaviv=enabled \
-  -Dc_args="-m32 -mfpmath=sse -msse -msse2 -mstackrealign" \
-  -Dcpp_args="-m32 -mfpmath=sse -msse -msse2 -mstackrealign" \
   --cross-file /root/cross_x86 \
   ..
 
@@ -71,13 +71,13 @@ ninja install
 cd /root
 
 # Build and install mesa
-git clone --depth=1 --branch mesa-24.3.3 https://gitlab.freedesktop.org/mesa/mesa.git
+git clone --depth=1 --branch mesa-25.3.3 https://gitlab.freedesktop.org/mesa/mesa.git
 cd mesa
 mkdir Build
 mkdir Build_x86
 
-export GALLIUM_DRIVERS="r300,r600,radeonsi,nouveau,virgl,svga,swrast,v3d,vc4,freedreno,etnaviv,tegra,lima,panfrost,zink,d3d12,asahi,iris"
-export VULKAN_DRIVERS="amd,broadcom,freedreno,panfrost,swrast,virtio,nouveau,intel,asahi"
+export GALLIUM_DRIVERS="asahi,d3d12,etnaviv,freedreno,iris,lima,llvmpipe,nouveau,panfrost,r300,r600,radeonsi,svga,tegra,v3d,vc4,virgl,zink"
+export VULKAN_DRIVERS="amd,broadcom,freedreno,intel,panfrost,swrast,virtio,nouveau,asahi"
 
 # Needed for rusticl
 rustup target add i686-unknown-linux-gnu
@@ -87,7 +87,7 @@ cd Build
 /root/meson/meson.py setup -Dprefix=/usr  -Dlibdir=/usr/lib \
   -Dbuildtype=release \
   -Db_ndebug=true \
-  -Dgallium-rusticl=true -Dopencl-spirv=true -Dshader-cache=enabled -Dllvm=enabled \
+  -Dgallium-rusticl=true -Dshader-cache=enabled -Dllvm=enabled \
   -Dgallium-drivers=$GALLIUM_DRIVERS \
   -Dvulkan-drivers=$VULKAN_DRIVERS \
   -Dplatforms=x11,wayland \
@@ -103,21 +103,18 @@ ninja install
 cd ../
 cd Build_x86
 
-# Asahi and Iris disabled because {asahi,intel}_clc fail to link to the correct libclang-cpp
-# Same issue with rusticl
-export GALLIUM_DRIVERS="r300,r600,radeonsi,nouveau,virgl,svga,swrast,v3d,vc4,freedreno,etnaviv,tegra,lima,panfrost,zink,d3d12"
-export VULKAN_DRIVERS="amd,broadcom,freedreno,panfrost,swrast,virtio"
+# Apparently meson doesn't know how to pass arguments to bindgen. Good job.
+export BINDGEN_EXTRA_CLANG_ARGS="--target=i686-unknown-linux-gnu"
+
 /root/meson/meson.py setup -Dprefix=/usr -Dlibdir=/usr/lib32 \
   -Dbuildtype=release \
   -Db_ndebug=true \
-  -Dgallium-rusticl=false -Dopencl-spirv=false -Dshader-cache=enabled -Dllvm=enabled \
+  -Dgallium-rusticl=true -Dshader-cache=enabled -Dllvm=enabled \
   -Dgallium-drivers=$GALLIUM_DRIVERS \
   -Dvulkan-drivers=$VULKAN_DRIVERS \
   -Dplatforms=x11,wayland \
   -Dfreedreno-kmds=msm,virtio,kgsl \
   -Dglvnd=enabled \
-  -Dc_args="-m32 -mfpmath=sse -msse -msse2 -mstackrealign" \
-  -Dcpp_args="-m32 -mfpmath=sse -msse -msse2 -mstackrealign" \
   --cross-file /root/cross_x86 \
   ..
 
